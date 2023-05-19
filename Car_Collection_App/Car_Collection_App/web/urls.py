797ddccb30cc818f3051b631a78147a02fad9d53
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-05-19 22:06:51 +0300

    added TODO in urls

--- Car_Collection_App/Car_Collection_App/web/urls.py
+++ Car_Collection_App/Car_Collection_App/web/urls.py
@@ -1,3 +1,15 @@
 urlpatterns = (
 
 )
+
+# TODO:
+# • http: // localhost: 8000 / - index page
+# • http: // localhost: 8000 / profile / create - profile create page
+# • http: // localhost: 8000 / catalogue / - catalogue page
+# • http: // localhost: 8000 / car / create / - car create page
+# • http: // localhost: 8000 / car / < car - id > / details / - car details page
+# • http: // localhost: 8000 / car / < car - id > / edit / - car edit page
+# • http: // localhost: 8000 / car / < car - id > / delete / - car delete page
+# • http: // localhost: 8000 / profile / details / - profile details page
+# • http: // localhost: 8000 / profile / edit / - profile edit page
+# • http: // localhost: 8000 / profile / delete / - profile delete page
