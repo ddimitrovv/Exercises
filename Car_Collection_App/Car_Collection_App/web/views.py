e7848727829dd8bf8142dc86d15a8e9ded3915ee
GalkaKG <galina_georgieva_net@abv.bg>
2023-05-22 09:54:17 +0300

    new commit

--- Car_Collection_App/Car_Collection_App/web/views.py
+++ Car_Collection_App/Car_Collection_App/web/views.py
@@ -29,13 +29,13 @@ def car_create(request):
     return render(request, 'car/car-create.html')
 
 
-def car_details(request):
+def car_details(request, pk):
     return render(request, 'car/car-details.html')
 
 
-def car_edit(request):
+def car_edit(request, pk):
     return render(request, 'car/car-edit.html')
 
 
-def car_delete(request):
+def car_delete(request, pk):
     return render(request, 'car/car-delete.html')
