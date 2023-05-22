49d20ab32bc8af50e869dd9fc358f66911f8d403
ddimitrovv <dimitrov.mitko.md@gmail.com>
2023-05-22 12:09:32 +0300

    added ProfileAdmin

--- Car_Collection_App/Car_Collection_App/web/admin.py
+++ Car_Collection_App/Car_Collection_App/web/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from Car_Collection_App.web.models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    ...
