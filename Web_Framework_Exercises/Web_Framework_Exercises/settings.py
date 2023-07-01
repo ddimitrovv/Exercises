bdcfef349d19b03ef9ab4b503b7dd9c00b56d4e1
GalkaKG <galina_georgieva_net@abv.bg>
2023-07-01 11:27:28 +0300

    created new app

--- Web_Framework_Exercises/Web_Framework_Exercises/settings.py
+++ Web_Framework_Exercises/Web_Framework_Exercises/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'Web_Framework_Exercises.web_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,9 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = (
-#     BASE_DIR / 'static',
-# )
+STATICFILES_DIRS = (
+    BASE_DIR / 'static',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ Web_Framework_Exercises/Web_Framework_Exercises/web_framework/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class WebFrameworkConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Web_Framework_Exercises.web_framework'
